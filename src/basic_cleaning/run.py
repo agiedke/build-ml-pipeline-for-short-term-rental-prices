c2851a39a554d092a27a370a5725570ed5e05be7
Sunanda Garg <sgarg@sunandas-air.speedport.ip>
2022-10-07 14:07:48 +0200

    fixing basic_cleaning -> filter on appropriate lon, lat

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,8 @@ def go(args):
     logger.info("Cleaning data")
     df = df[df["price"].between(args.min_price, args.max_price)].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     logger.info("Saving file")
     df.to_csv(args.output_artifact, index=False, index_label=False)
